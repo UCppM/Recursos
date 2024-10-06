eec8899892201635eeeea978709fbb231992f6e7
ayhon <ayhonnick@gmail.com>
2024-10-06 10:25:28 +0200

    fix: simplify freefood.py oneliner

--- SolucionesConcursos/ConcursoIntroduccion2024/.oneliners/freefood.py
+++ SolucionesConcursos/ConcursoIntroduccion2024/.oneliners/freefood.py
@@ -1,1 +1,1 @@
-print(len({d for _ in range(int(input())) for d in (lambda ls: set(range(ls[0],ls[1]+1)))([int(x) for x in input().split()])}))
+print(len({d for _ in range(int(input())) for d in (lambda ls: range(ls[0],ls[1]+1))(list(map(int,input().split())))}))
