3b2d17e031a237e1a9d77f296ba25b202cf9b020
Daniel Gonzalez Arbelo <dgonzarb@gmail.com>
2024-10-19 13:17:56 +0200

    Soluciones Liga 1 Ronda 1 en sucio

---
+++ SolucionesConcursos/2 - Liga 1 Ronda 1/1878A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#pragma warning (disable : 4996)
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define mp make_pair
+
+const double EPS = 1e-9;
+const double INF = 1e9; //1e18 tambien suele ir bien
+using ll = long long int;
+using ull = unsigned long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using vvii = vector<vii>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using db = double;
+using vdb = vector<db>;
+using ldb = long double; //100 ceros pero poca precision decimal
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t; cin >> t;
+    while(t--) {
+        int n, a; cin >> n >> a;
+        bool aparece = false;
+        for(int i = 0; i < n; i++) {
+            int b; cin >> b;
+            if(b == a) aparece = true;
+        }
+        if(aparece) cout << "YES\n";
+        else cout << "NO\n";
+    }
+    return 0;
+}
---
+++ SolucionesConcursos/2 - Liga 1 Ronda 1/96A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#pragma warning (disable : 4996)
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define fi first
+#define se second
+#define mp make_pair
+
+const double EPS = 1e-9;
+const double INF = 1e9; //1e18 tambien suele ir bien
+using ll = long long int;
+using ull = unsigned long long;
+using ii = pair<int, int>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vii = vector<ii>;
+using vvii = vector<vii>;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using db = double;
+using vdb = vector<db>;
+using ldb = long double; //100 ceros pero poca precision decimal
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    string s; cin >> s;
+    char cur = 'a';
+    int count = 0;
+    bool dangerous = false;
+    for(char c : s) {
+        if(c == cur) count++;
+        else {
+            cur = c;
+            count = 1;
+        }
+        if(count == 7) dangerous = true;
+    }
+    if(dangerous) cout << "YES\n";
+    else cout << "NO\n";
+    return 0;
+}
